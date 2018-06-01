9c080ac777bafb85b4aabf399392fcf8f0e79d8a
Michel Paupulaire <mpaupulaire@rms.com>
2018-06-01 14:27:51 -0600

    a little refactoring

--- App/components/Notification.js
+++ App/components/Notification.js
@@ -5,72 +5,77 @@ import {
   Animated,
   Easing,
 } from 'react-native';
-import { LinearGradient } from 'expo'
+import LinearGradient from 'react-native-linear-gradient';
+
+const styles = {
+  outerContainer: {
+    position: 'absolute',
+    left: 10,
+    top: 50,
+    alignSelf: 'center',
+
+  },
+  container: {
+    width: 350,
+    borderRadius: 4,
+    borderWidth: 1,
+    borderColor: '#870904',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    elevation: 10,
+    paddingHorizontal:15,
+    paddingVertical: 10,
+
+  },
+  errorText: {
+    color: 'white',
+    fontSize: 20,
+    textShadowColor: '#5b0000',
+    textShadowRadius: 2,
+    textShadowOffset: { width: 1, height: 1 }
+  }
+}
+
 
 export default class Notification extends Component {
-  constructor(props) {
-    super(props)
-    this.animatedValue = new Animated.Value(0);
-    this.animate = this.animate.bind(this)
-    this.animationDuration = this.props.timeout || 3500
-    this.state = { animate: true }
+  static defaultProps = {
+    timeout: 3500,
+    message: 'Sorry, something went wrong!'
   }
+
+  state = {
+    value: new Animated.Value(0)
+  }
+
   componentDidMount() {
     this.animate()
   }
+
   animate() {
-    this.animatedValue.setValue(0)
+    this.state.value.setValue(0)
     Animated.timing(
-      this.animatedValue, {
+      this.state.value, {
         toValue: 1,
-        duration: this.animationDuration,
+        duration: this.props.timeout,
         easing: Easing.linear
       }
     ).start()
   }
-  styles = {
-    outerContainer: {
-      position: 'absolute',
-      left: 10,
-      top: 50,
-      alignSelf: 'center',
-
-    },
-    container: {
-      width: 350,
-      borderRadius: 4,
-      borderWidth: 1,
-      borderColor: '#870904',
-      flexDirection: 'column',
-      justifyContent: 'center',
-      alignItems: 'center',
-      elevation: 10,
-      paddingHorizontal:15,
-      paddingVertical: 10,
-      
-    },
-    errorText: {
-      color: 'white',
-      fontSize: 20,
-      textShadowColor: '#5b0000',
-      textShadowRadius: 2,
-      textShadowOffset: { width: 1, height: 1 }
-    }
-
-  }
   render() {
-    const marginLeft = this.animatedValue.interpolate({
+    const marginLeft = this.state.value.interpolate({
       inputRange: [0, .05, .95, 1],
       outputRange: [-350, 15, 15, -350]
     })
     return (
-      <Animated.View style={[this.styles.outerContainer, { left: marginLeft }]} >
+      <Animated.View style={[styles.outerContainer, { left: marginLeft }]} >
         <LinearGradient
           colors={['#a8241f', '#820f0f']}
           start={[0.95, 0.2]}
           end={[0.1, 0.4]}
-          style={this.styles.container}>
-          <Text style={this.styles.errorText}>{this.props.message || "Sorry, something went wrong!"}</Text>
+          style={styles.container}
+        >
+          <Text style={styles.errorText}>{this.props.message}</Text>
         </LinearGradient>
       </Animated.View>
     )
